9c1800e132e491ef3caddf4a30342e474aa5580c
Kyung Han Song <kyungsong@Kyungs-Air.fios-router.home>
2018-01-16 23:57:15 -0500

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const path = require('path'); // built in module, no need for npm install
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public'); //Path is /Users/kyungsong/projects/node-chat-app/public
 const port = process.env.PORT || 3000;                                                      
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); 
 
+// middleware
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});// let you register an event listener , when connection comes in order to do something, you provide a callback
 
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
-module.exports = {app}
+module.exports = {app};
