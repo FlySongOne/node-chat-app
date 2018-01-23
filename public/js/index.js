b580f9fb57bccb0304c341b61582433d74d69836
Kyung Han Song <kyungsong@Kyungs-Air.fios-router.home>
2018-01-23 18:04:08 -0500

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,9 +30,14 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextBox = $('[name=message]');
+
   socket.emit('createMessage', {
   	from: 'User',
-  	text: $('[name=message]').val()
+  	text: messageTextBox.val()
+  }, function () {
+  	//acknowledgement callback
+  	messageTextBox.val('')
   });
 });
 
@@ -42,14 +47,17 @@ locationButton.on('click', function() {
   	return alert('Geolocation not supported by your browser.');
   } 
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
-  
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
 
   }, function () {
+  	locationButton.removeAttr('disabled');
   	alert('Unable to fetch location.');
   });
 });
